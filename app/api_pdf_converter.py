8e6634cfd14986bec6cf29a047cf5f1d021121bb
Ulises Jimenez <ulisesjimenez@google.com>
2024-08-21 20:11:54 -0600

    initial commit

---
+++ app/api_pdf_converter.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from google.cloud import storage
+
+import os
+import subprocess
+
+PROJECT_ID = 'gsd-ai-mx-ulises'
+storage_client = storage.Client(project=PROJECT_ID)
+
+class Item(BaseModel):
+    input_bucket: str
+    input_file: str 
+    output_bucket: str
+    output_file: str | None = None
+
+
+app = FastAPI()
+
+def convert_file_to_pdf(input_file):
+    subprocess.run(
+        f'sudo libreoffice \
+        --convert-to pdf \
+         {input_file}', shell=True)    
+    output_file_name = os.path.basename(input_file)
+    output_file_name = f'{output_file_name.split(".")[0]}.pdf'
+    if os.path.exists(output_file_name):
+        return output_file_name
+    else:
+        return None
+    
+def download_storage_tmp(item: Item):
+    input_bucket = storage_client.bucket(item.input_bucket)
+    input_blob = input_bucket.blob(item.input_file)
+    input_file_name = os.path.basename(item.input_file) 
+    input_blob.download_to_filename(input_file_name)    
+    return input_file_name
+
+def upload_output(item: Item,output_file_path):
+    output_bucket = storage_client.bucket(item.output_bucket)
+    output_blob = output_bucket.blob(item.output_file)
+    output_blob.upload_from_filename(output_file_path)
+    
+@app.post("/convert2pdf")
+async def create_item(item: Item): 
+
+    input_file_name = download_storage_tmp(item)
+    output_file_name = convert_file_to_pdf(input_file_name)
+   
+    upload_output(item,output_file_name)
+    
+    os.remove(input_file_name)
+    os.remove(output_file_name)
+    
+    return {"message": "File converted and copied successfully"}
